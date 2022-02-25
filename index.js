8baa0344fcbae060baadd8b2ae815c54340c07bb
vipin sati <vipin.zversal@gmail.com>
2022-02-25 13:15:55 +0530

    new calcultor

---
+++ index.js
@@ -0,0 +1,91 @@
+function getHistory(){
+    return document.getElementById("history-value").innerText;
+}
+ function printHistory(num){
+     document.getElementById("history-value").innerText=num;
+ }
+ 
+function getOutput(num){
+     return document.getElementById("output-value").innerText;
+}
+function printOutput(num){
+    if(num==""){
+        document.getElementById("output-value").innerText=num;
+    }
+    else{
+    document.getElementById("output-value").innerText=getFormattedNumber(num);
+}
+}
+function getFormattedNumber(num){
+    if(num=="-"){
+        return "";
+    }
+    var n= Number(num);
+    var value = n.toLocaleString("en");
+    return value;
+}
+ function reverseNumberFormat(num){
+     return Number(num.replace(/,/g,''))
+ }
+ var operator=document.getElementsByClassName("operator");
+ for(var index=0;index<operator.length;index++){
+     operator [index].addEventListener('click',function(){
+       if(this.id=="clear"){
+           printHistory("");
+           printOutput("");
+       }
+       else if(this.id=="backspace"){
+           var output=reverseNumberFormat(getOutput()).toString();
+           if(output){
+               output=output.substr(0,output.length-1);
+               printOutput(output);
+           }
+       }
+       else{
+           var output=getOutput();
+           var history=getHistory();
+           if(output==""&&history!=""){
+               //condition ? true/false//
+               if(isNaN(history[history.length-1])){
+                   history=history.substr(0,history.length-1);
+               }
+               }
+               
+               if (output!=""|| history!=""){
+                output=output==""?
+                output:reverseNumberFormat(output);
+                history=history+output;
+                if(this.id=="="){
+                    var result=eval(history);
+                   printOutput(result);
+                   printHistory("");
+
+                }
+                else {
+                    history=history+this.id;
+                    printHistory(history);
+                    printOutput("");
+                
+            }
+                   
+            }
+               
+       }
+
+
+     })     
+ }
+var number=document.getElementsByClassName("number");
+for(var index=0;index<number.length;index++){
+    number[index].addEventListener('click',function(){
+        var output=reverseNumberFormat(getOutput());
+        if(output!=NaN)//if output is a number}
+            output=output+this.id;
+            printOutput(output);
+    })
+}
+
+
+
+
+
